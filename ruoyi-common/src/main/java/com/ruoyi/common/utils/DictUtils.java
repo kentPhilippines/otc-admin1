fff78232a2b9912611e5e101e480ce071277c249
MagicJson <it_magicjson@163.com>
2024-07-12 15:28:03 +0800

    1.DateUtils提升parseDateToStr性能与安全性 2.ExceptionUtil使用try-with-resources关闭需要关闭的流 3.解决使用@Excel字典值属性时，导入导出功能中当用户上送字典值会导致使用该字典值的字段设置不了希望设置的值

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/DictUtils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/DictUtils.java
@@ -1,9 +1,14 @@
 package com.ruoyi.common.utils;
 
-import java.util.List;
-import org.springframework.stereotype.Component;
 import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.domain.entity.SysDictData;
+import com.ruoyi.common.utils.reflect.ReflectUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 字典工具类
@@ -17,7 +22,8 @@ public class DictUtils
      * 分隔符
      */
     public static final String SEPARATOR = ",";
-
+    public static final String DICT_LABEL = "dictLabel";
+    public static final String DICT_VALUE = "dictValue";
     /**
      * 设置字典缓存
      * 
@@ -79,7 +85,7 @@ public class DictUtils
 
     /**
      * 根据字典类型和字典值获取字典标签
-     * 
+     *
      * @param dictType 字典类型
      * @param dictValue 字典值
      * @param separator 分隔符
@@ -87,37 +93,7 @@ public class DictUtils
      */
     public static String getDictLabel(String dictType, String dictValue, String separator)
     {
-        StringBuilder propertyString = new StringBuilder();
-        List<SysDictData> datas = getDictCache(dictType);
-        if (StringUtils.isNull(datas))
-        {
-            return StringUtils.EMPTY;
-        }
-        if (StringUtils.containsAny(dictValue, separator))
-        {
-            for (SysDictData dict : datas)
-            {
-                for (String value : dictValue.split(separator))
-                {
-                    if (value.equals(dict.getDictValue()))
-                    {
-                        propertyString.append(dict.getDictLabel()).append(separator);
-                        break;
-                    }
-                }
-            }
-        }
-        else
-        {
-            for (SysDictData dict : datas)
-            {
-                if (dictValue.equals(dict.getDictValue()))
-                {
-                    return dict.getDictLabel();
-                }
-            }
-        }
-        return StringUtils.stripEnd(propertyString.toString(), separator);
+       return getDictElement(dictType, dictValue, separator, DICT_LABEL);
     }
 
     /**
@@ -130,37 +106,89 @@ public class DictUtils
      */
     public static String getDictValue(String dictType, String dictLabel, String separator)
     {
-        StringBuilder propertyString = new StringBuilder();
-        List<SysDictData> datas = getDictCache(dictType);
-        if (StringUtils.isNull(datas))
-        {
+        return getDictElement(dictType, dictLabel, separator,DICT_VALUE);
+    }
+
+
+    /**
+     * 根据字典类型和字典条件获取字典内容
+     *
+     * @param dictType 字典类型
+     * @param dictCondition 字典条件（字典标签或字典值）
+     * @param separator 分隔符
+     * @return 字典内容
+     */
+    public static String getDictElement(String dictType, String dictCondition
+            , String separator, String filedAttr) {
+        // 获取字典缓存
+        List<SysDictData> dictData = getDictCache(dictType);
+        if (StringUtils.isNull(dictData)) {
             return StringUtils.EMPTY;
         }
-        if (StringUtils.containsAny(dictLabel, separator))
-        {
-            for (SysDictData dict : datas)
-            {
-                for (String label : dictLabel.split(separator))
-                {
-                    if (label.equals(dict.getDictLabel()))
-                    {
-                        propertyString.append(dict.getDictValue()).append(separator);
-                        break;
-                    }
-                }
+		// 处理单个标签的情况
+        if (!StringUtils.containsAny(dictCondition, separator)) {
+            return getSingleDictElement(dictData, dictCondition, filedAttr);
+        }
+        // 处理多个标签的情况
+        return getMultipleDictElements(dictData, dictCondition, separator, filedAttr);
+    }
+    /**
+     * 获取字典元素
+     *
+     * @param dictData    字典数据列表
+     * @param dictCondition 条件值
+     * @param dictAttr     字典属性
+     * @return 符合条件的字典属性值，未找到返回空字符串
+     */
+    private static String getDictElement(List<SysDictData> dictData, String dictCondition, String dictAttr) {
+        // 遍历字典数据
+        for (SysDictData dict : dictData) {
+            // 判断需要转换的字典属性是DICT_LABEL还是DICT_VALUE
+            String dictTransV = Objects.equals(DICT_LABEL, dictAttr) ? DICT_VALUE : DICT_LABEL;
+            // 获取转换后的字典属性值
+            String retValue = ReflectUtils.invokeGetter(dict, dictTransV);
+            // 如果转换后的值和条件值相等，则返回所需的字典属性值
+            if (Objects.equals(dictCondition, retValue)) {
+                return ReflectUtils.invokeGetter(dict, dictAttr);
             }
         }
-        else
-        {
-            for (SysDictData dict : datas)
-            {
-                if (dictLabel.equals(dict.getDictLabel()))
-                {
-                    return dict.getDictValue();
-                }
-            }
+        return StringUtils.EMPTY; // 没有找到匹配的标签，返回空字符串
+    }
+
+    /**
+     * 获取单个字典元素
+     *
+     * @param dictData     字典数据列表
+     * @param dictCondition 条件值
+     * @param dictAttr      字典属性
+     * @return 符合条件的字典属性值
+     */
+    private static String getSingleDictElement(List<SysDictData> dictData, String dictCondition, String dictAttr) {
+        // 如果字典属性是DICT_VALUE并且条件是数字，则直接返回条件
+        if (Objects.equals(DICT_VALUE, dictAttr) && StringUtils.isNumeric(dictCondition)) {
+            return dictCondition;
         }
-        return StringUtils.stripEnd(propertyString.toString(), separator);
+        return getDictElement(dictData, dictCondition, dictAttr);
+    }
+
+    /**
+     * 处理多个标签的情况
+     *
+     * @param dictData     字典数据列表
+     * @param dictCondition 标签条件
+     * @param separator    分隔符
+     * @param dictAttr     字典属性
+     * @return 拼接的字典值
+     */
+    private static String getMultipleDictElements(List<SysDictData> dictData, String dictCondition, String separator, String dictAttr) {
+        // 分割条件值，得到多个条件
+        String[] conditions = dictCondition.split(separator);
+
+        // 使用Stream处理多个条件
+        return Stream.of(conditions)
+                .map(condition -> getDictElement(dictData, condition, dictAttr))
+                .filter(StringUtils::isNotEmpty) // 过滤掉空值
+                .collect(Collectors.joining(separator)); // 拼接结果
     }
 
     /**
