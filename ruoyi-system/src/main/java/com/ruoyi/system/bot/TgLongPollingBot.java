2d4c2d61e480d647033cff766cfbf8d7c5a7106e
dorion <872354125@qq.com>
2024-07-16 00:18:20 +0800

    准备做ws短信批量上传

--- ruoyi-system/src/main/java/com/ruoyi/system/bot/TgLongPollingBot.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/bot/TgLongPollingBot.java
@@ -4,12 +4,11 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-@Component
+//@Component
 @Slf4j
 public class TgLongPollingBot extends TelegramLongPollingBot {
 
