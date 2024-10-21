f6749f59250795a4147cea835087bd35ea9c210e
CZHXX <14938243+czhxx@user.noreply.gitee.com>
2024-10-21 02:42:59 +0000

    rename ruoyi-system/src/main/javajava to ruoyi-system/src/main/java.java.
    Signed-off-by: CZHXX <14938243+czhxx@user.noreply.gitee.com>

---
+++ ruoyi-system/src/main/java.java
@@ -0,0 +1,39 @@
+
+
+/**
+ * 定义一个累
+ */
+public class Java {
+    /**
+     * 三个与类名相关的属性
+      */
+    private String name1;
+    private String name2;
+
+
+
+    /**
+     * @param name
+     * @param name2
+     */
+    public Java(String name, String name2) {
+        this.name1 = name1;
+        this.name2 = name2;
+
+    }
+
+
+
+    /**
+     * main函数创建对象并调用两个方法
+     */
+
+   public void java() {
+       System.out.println(name1 + "cc");
+   }
+    public void sha() {
+        System.out.println(name2 + "xx");
+    }
+}
+
+
